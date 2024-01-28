387025eba8f9d937dddd826495ecc9052c2705f8
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-01-27 23:08:46 -0500

    fixing character controller

--- game/scripts/CustomerController.cs
+++ game/scripts/CustomerController.cs
@@ -53,8 +53,8 @@ public partial class CustomerController : CharacterBody3D
 		if (EnableFloppy != floppyPrevState)
 		{
 			SetFloppy(EnableFloppy);
-			demandContainer.QueueFree();
-			demand.QueueFree();
+			demandContainer?.QueueFree();
+			demand?.QueueFree();
 		}
 
 		floppyPrevState = EnableFloppy;
