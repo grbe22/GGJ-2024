ecf62a4d1f55eb907d03c5c59fe3c30ac730f128
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-01-28 12:19:59 -0500

    Create Scoring.cs
    Basic algorithm for generating a score (0-100) based on two inputs

---
+++ game/scripts/Scoring.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class Scoring : Node
+{
+	public Scoring() {}
+	
+	private int GradeDrink(int request, int provided) {
+		if (request == provided) {
+			return 2;
+		}
+		if (request == 5 && (provided == 4 || provided == 2)) { return 1; }
+		if (request == 3 && (provided == 1 || provided == 2)) { return 1; }
+		return 0
+	}
+	
+	public int Grade(int[] order, int[] provided) {
+		
+		int finalScore = 0;
+		// element 0 is bowl, 1 is cup, 2 is addon.
+		if (order[2] == 0) {
+			
+			// case they want a plain drink
+			int x = GradeDrink(order[1], provided[1]);
+			finalScore += x * 35;
+			
+		} else {
+			
+			// case they just want a topped drink
+			if (provided[2] == order[2]) {
+				finalScore += 20;
+			}
+			int x = GradeDrink(order[1], provided[1]);
+			finalScore += x * 25;
+			
+		}
+		
+		if (order[0] == provided[0]) {
+			finalScore = 30;
+		}
+		
+		return finalScore;
+	}
+}
