9174cbe75c594c83499313bd7fba5e6f626a9e24
Sarah Schneider (RIT Student) <ses1431@rit.edu>
2024-01-27 22:15:10 -0500

    move minus vector math

--- game/scripts/GrabStuff.cs
+++ game/scripts/GrabStuff.cs
@@ -1,32 +1,50 @@
 using Godot;
 using System;
 
-public partial class GrabStuff : Node
+public partial class GrabStuff : StaticBody3D
 {
-	public Node heldItem;
-	public bool hoverCup;
+	private StaticBody3D heldItem;
+	private bool hoverCup = false;
+	private Vector3 offset;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		offset = GetNode<StaticBody3D>("%Cup").Position;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("click") && hoverCup) {
-			// todo: spawn cup at mouse position
+		if (Input.IsActionJustPressed("click")) {
+			if (heldItem != null)
+			{
+				GD.Print("drop", heldItem.Position);
+				heldItem = null;
+			}
+			else if (hoverCup)
+			{
+				heldItem = GetNode<StaticBody3D>("%Cup");
+				GD.Print("pickup");
+			}
 		}
-		//if (Input.IsActionJustPressed("Switch Camera") && Input.IsActionPressed("Switch Camera"))
-		//{
-			//switchView();
-		//}
+		else if (heldItem != null) {
+			Vector2 mousePos = GetViewport().GetMousePosition();
+			Vector3 self = new Vector3(mousePos[0] / 100, mousePos[1] / 100, offset[2]);
+			
+			heldItem.Position = self;
+			GD.Print("move", mousePos);
+		}
+		else {
+			GD.Print(offset);
+		}
 	}
 	
-	private void _on_coffee_cup_mouse_entered() {
+	private void _on_self_mouse_entered() {
 		hoverCup = true;
 	}
 	
-	private void _on_coffee_cup_mouse_exited() {
+	private void _on_self_mouse_exited() {
 		hoverCup = false;
 	}
 }
